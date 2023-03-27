324f0f1b617aa9a4036f8804d96899c0354b82ef
devsince2021 <dev.ryu2021@gmail.com>
2023-03-27 23:06:57 +0900

    #user - 컨트롤러 테스트 추가

--- src/users/controllers/users.controller.spec.ts
+++ src/users/controllers/users.controller.spec.ts
@@ -1,12 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateUserDto } from '../dtos/createUser.dto';
+import { User } from '../entities/user.entity';
+
+import { UsersService } from '../services/users.service';
 import { UsersController } from './users.controller';
 
-describe('UsersController', () => {
+describe('CarController', () => {
   let controller: UsersController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +19,26 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('[Post] createUser', () => {
+    it('should return the User when receives a proper createUserDto', async () => {
+      const properDto: CreateUserDto = {
+        email: 'test@gmail.com',
+        name: 'test',
+        password: '1234',
+      };
+
+      const user = await controller.createUser(properDto);
+
+      expect(user).toBeInstanceOf(User);
+      expect(user).toHaveProperty('id');
+      expect(user).toHaveProperty('isActive');
+      expect(user).toHaveProperty('phone');
+      expect(user).toHaveProperty('role');
+      expect(user).toHaveProperty('signInType');
+    });
+
+    // it('should throw an exception when services has problem', () => {});
   });
 });
