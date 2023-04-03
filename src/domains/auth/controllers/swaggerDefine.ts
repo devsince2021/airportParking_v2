997e694fc34201800c322411b4544ab9e6303fe7
devsince2021 <dev.ryu2021@gmail.com>
2023-04-04 01:34:52 +0900

    #auth - Update swagger

--- src/domains/auth/controllers/swaggerDefine.ts
+++ src/domains/auth/controllers/swaggerDefine.ts
@@ -1,4 +1,5 @@
 import { SendVerifyCodeResDto } from '../dtos/auth.sendVerifyCodeDto';
+import { VerifyCodeResDto } from '../dtos/auth.verifyCodeDto';
 
 export const TAG = 'Auth';
 
@@ -8,11 +9,19 @@ export const OPERATION = {
     description:
       '휴대폰 번호 인증에 필요한 4자리 코드를 문자로 전송하는 api 입니다.',
   },
+  verifyCode: {
+    summary: '휴대폰 인증 번호 검증 API',
+    description: '유효한 인증번호 인지 확인하는 api 입니다.',
+  },
 };
 
 export const RESPONSE = {
   sendVerifyCode: {
     description: '코드 발송 성공 여부',
     type: SendVerifyCodeResDto,
+  },
+  verifyCode: {
+    description: '유효한 코드인지 확인 결과',
+    type: VerifyCodeResDto,
   },
 };
