c45e4d9723d5535744762be0b47e404600189984
devsince2021 <dev.ryu2021@gmail.com>
2023-04-15 17:12:37 +0900

    #0 - todo

--- src/domains/reservations/controllers/reservations.controller.ts
+++ src/domains/reservations/controllers/reservations.controller.ts
@@ -29,6 +29,16 @@ export class ReservationsController {
     return { url, uploadKey };
   }
 
+  // todo1 - 예약 변경하기 api v1에서 가져오기
+  // todo2 - company 만들어서 예약에 물리기
+
+  /** todo3
+   * 하위 레이어에서 발생하는 문제는 에러코드와 메세지 객체를 throw 로 올린다.
+   * 컨트롤러에서 캐치로 잡아서 필요한 핸들링 제공? 혹은 그냥 던지면 글로벌 익셉션 핸들러가 해주나?
+   * 에러 코드 정리하기
+   * 코드 메세지 함께 던질수잇는 커스텀 배드 리퀘스트 만들기
+   */
+
   @Post()
   @UseInterceptors(FileInterceptor('excel')) // todo: 'excel'을 RESERVATION_UPLOAD_KEY로 대체
   async createReservation(
